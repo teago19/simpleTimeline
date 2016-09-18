5c4dfc51625c8ae17b699ee592c67de82da7b240
Thiago Soares <godaum19@gmail.com>
2016-09-18 15:46:00 -0300

    Showing avatar's name and image

--- js/script.js
+++ js/script.js
@@ -1,12 +1,24 @@
-var posts;
-
 $.ajax({
     dataType: "json",
     url: "timeline.json",
     mimeType: "application/json",
     success: function(resultJson){
-        posts = resultJson
-		console.log(JSON.stringify(posts));
+        var posts = resultJson
+		console.log(posts.length);
+
+		for (var i = 0; i < posts.length; i++) {
+			//POST//
+			var node = document.createElement("div");
+			node.className = "col-sm-8 col-sm-offset-4 post";
+
+			//AVATAR//
+			var avatar = document.createElement("div");
+			avatar.className = "avatar col-sm-4";
+			avatar.innerHTML = '<img src="' + posts[i].user.picture + '" class="img-circle imgAvatar">' + 
+								'<span class="nomeAvatar">' + posts[i].user.username + '</span>';
+			document.getElementById("timeline").appendChild(node).appendChild(avatar);
+
+		}
     }
 });
 
