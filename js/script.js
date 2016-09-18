6b03ad34e4e79e05e2771f1fa849386e6de5bf4f
Thiago Soares <godaum19@gmail.com>
2016-09-18 17:32:12 -0300

    post content avaliable

--- js/script.js
+++ js/script.js
@@ -9,14 +9,23 @@ $.ajax({
 		for (var i = 0; i < posts.length; i++) {
 			//POST//
 			var node = document.createElement("div");
-			node.className = "col-sm-8 col-sm-offset-4 post";
+			node.className = "col-sm-8 col-sm-offset-4";
+
+			var divRow = document.createElement("div");
+			divRow.className = "row media panel panel-default";
 
 			//AVATAR//
 			var avatar = document.createElement("div");
-			avatar.className = "avatar col-sm-4";
+			avatar.className = "avatar col-sm-3 media-left";
 			avatar.innerHTML = '<img src="' + posts[i].user.picture + '" class="img-circle imgAvatar">' + 
-								'<span class="nomeAvatar">' + posts[i].user.username + '</span>';
-			document.getElementById("timeline").appendChild(node).appendChild(avatar);
+								'<div class="avatarInfo"> <span class="avatarUsername">' + posts[i].user.username + '</span> </div>';
+			document.getElementById("timeline").appendChild(node).appendChild(divRow).appendChild(avatar);
+
+			//Post//
+			var post = document.createElement("div");
+			post.className = 'post col-sm-9';
+			post.innerHTML = '<span class="postContent">' + posts[i].content + '</span>';
+			document.getElementById("timeline").appendChild(node).appendChild(divRow).appendChild(post);
 
 		}
     }
